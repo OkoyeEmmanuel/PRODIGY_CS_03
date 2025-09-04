0dd5eb51ca3b091ceed085b2f1b1b242298cc6dc
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-04 23:59:41 +0200

    Update password_checker.py
    The length is working but not the checking of uppercase and lowercase

--- password_checker.py
+++ password_checker.py
@@ -2,43 +2,50 @@ import time
 global bPassword
 bPassword=False
 
+print("#############################################################################")
+print("Welcome to the Password Checker!")
 print("1. Ensure your password is at least 8 characters long.")
 time.sleep(2)
 
 print("2. Ensure your password contains both uppercase and lowercase letters.")
-time.sleep(2)
+time.sleep(1)
 
 print("3. Ensure your password contains at least one digit.")
-time.sleep(2)
+time.sleep(1)
 
 print("4. Ensure your password contains at least one special character (e.g., !, @, #, $, etc.).")
-time.sleep(2)
+print("#############################################################################")
+time.sleep(1)
 
 
 def check_password_length(password):
     return len(password) >= 8
    
+def check_upper_and_lower(password):
+    upper = any(c.isupper for c in password)
+    lower = any(c.islower for c in password)
+    return upper and lower # returns true only if upper and lower are true
 
 password_input = input("Enter your password:")
+print("Checking password...")
+time.sleep(3)
 
-print("Checking your password length...")
-check_password_length(password_input)
+if check_password_length(password_input) == False:
+    print("* Your password is too short.")
+else:
+    print("# Your password length is sufficient.")
+
+if check_upper_and_lower(password_input) == True:
+    print("# Your password contains both uppercase and lowercase letters.")
+else:
+    print("* Your password does not contain both uppercase and lowercase letters.")
+
+
+
+
 time.sleep(1)
-
-print("Checking for uppercase and lowercase letters...")
-
-time.sleep(1)
-
-print("Checking for at least one digit...")
-
-time.sleep(1)
-
-print("Checking for at least one special character...")
-time.sleep(1)
-
-
 print("Password check complete.")
-time.sleep(1)
+time.sleep(2)
 print("Password Review:")
 
 
