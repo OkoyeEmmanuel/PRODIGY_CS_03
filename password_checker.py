84fd7060e6abb782531be12ecd894044554051c1
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-04 23:36:41 +0200

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,7 +1,6 @@
 import time
-
-
-    
+global bPassword
+bPassword=False
 
 print("1. Ensure your password is at least 8 characters long.")
 time.sleep(2)
@@ -16,8 +15,33 @@ print("4. Ensure your password contains at least one special character (e.g., !,
 time.sleep(2)
 
 
+def check_password_length(password):
+    return len(password) >= 8
+   
+
 password_input = input("Enter your password:")
+
 print("Checking your password length...")
+check_password_length(password_input)
 time.sleep(1)
 
-print(len(password_input))
+print("Checking for uppercase and lowercase letters...")
+
+time.sleep(1)
+
+print("Checking for at least one digit...")
+
+time.sleep(1)
+
+print("Checking for at least one special character...")
+time.sleep(1)
+
+
+print("Password check complete.")
+time.sleep(1)
+print("Password Review:")
+
+
+if check_password_length(password_input):
+    bPassword=True
+    print("Your password meets the length requirement.")
